a21bb8ccd6f852c171501d8a015e8cf02c0942b6
Danil Adelgareev <danil.adel2014@gmail.com>
2024-04-26 22:09:29 +0500

    second commit

--- taskwindow6.cpp
+++ taskwindow6.cpp
@@ -18,8 +18,8 @@ void taskwindow6::on_pushButton_clicked()
     QString text = ui->textEdit->toPlainText(); // Получаем текст из QTextEdit
 
     std::regex pattern1(R"((.*)01(.*))"); // (*)01*
-    std::regex pattern2("(?:101|001){1}");    // (0+1)01
-    std::regex pattern3(R"(00(0+1)*)");   // 00(0+1)*
+    std::regex pattern2(R"((^|[^0-9])(101|001)([^0-9]|$))");   // (0+1)01
+    std::regex pattern3(R"(^(001|000).*)");   // 00(0+1)*
 
 
     if (containsInvalidCharacters(text)) {
